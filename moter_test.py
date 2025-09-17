5d2dc6b18d2fb487b3f57b96cfedcc3f1ef15ef1
Rishabh Jain <2r10j5@gmail.com>
2025-09-18 03:20:06 +0530

    Delete moter_test.py

--- moter_test.py
+++
@@ -1,9 +0,0 @@
-from brickpi3 import BrickPi3
-import time
-
-BP = BrickPi3()
-DEFAULT_SPEED = 2000  # RPM
-BP.set_motor_dps(BP.PORT_A, DEFAULT_SPEED)  # Convert RPM to degrees/sec
-time.sleep(5)
-BP.set_motor_power(BP.PORT_A, 0)
-BP.reset_all()
